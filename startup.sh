0a6fff8d1e033c3a7a4ac17e802fd89fd5b61491
Erika Pauwels <erika.pauwels@tenforce.com>
2015-08-27 10:59:04 +0200

    Link the feeds folder in public/ext

--- startup.sh
+++ startup.sh
@@ -4,18 +4,25 @@
 rm /app/index.php
 rm /app/logo.png
 
+# Remove old symbolic links
+rm /home/datahub/apps/default/public 2>/dev/null
+rm /app/tdt-core/public/ext/feeds 2>/dev/null
+rm /app/tdt-core/public/ext 2>/dev/null
+rm /home/datahub/live/crons 2>/dev/null
+
 # Make feeds folder accessible to TDT
 chown -R www-data:www-data /home/datahub/feeds/
 chmod 2755 -R /home/datahub/feeds/
 
 # Link TDT public folder to Apache root config
 mkdir -p /home/datahub/apps/default
-rm /home/datahub/apps/default/public 2>/dev/null
 ln -s /app/tdt-core/public /home/datahub/apps/default/
 
 # Link external folder in TDT public folder
-rm /app/tdt-core/public/ext 2>/dev/null
 ln -s /app/datahub-config/ext /app/tdt-core/public
+
+# Link feeds folder in TDT public/ext folder
+ln -s /home/datahub/feeds /app/tdt-core/public/ext/
 
 # Make .htaccess of the ext folder availabe to TDT
 chown www-data:www-data /app/datahub-config/ext/.htaccess
@@ -25,7 +32,6 @@ chmod 644 /home/datahub/feeds/.htpasswd
 
 # Link cron jobs
 mkdir -p /home/datahub/live
-rm /home/datahub/live/crons 2>/dev/null
 ln -s /app/datahub-config/crons /home/datahub/live/
 
 # Make cron jobs executable
